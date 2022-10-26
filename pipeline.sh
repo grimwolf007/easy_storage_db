9c4dd4fec373d08a6c1aaba00310ed2c6c8ae61c
grimwolf007 <user@email.invaild>
2022-10-26 22:49:52 +0000

    fixed pipeline and added to .gitignore

--- pipeline.sh
+++ pipeline.sh
@@ -1,27 +1,115 @@
-echo Adding to Build Archive
+#!/bin/bash
+
+#Colors
+RESET='\033[0m'
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+LRED='\033[1;31m'
+
+info(){
+       	echo -e "${CYAN}${@}${RESET}"
+}
+
+pass(){
+       	echo -e "${GREEN}${@}${RESET}"
+}
+
+warn(){
+       	echo -e "${YELLOW}${@}${RESET}"
+}
+
+fail(){
+       	echo -e "${LRED}${@}${RESET}"
+}
+
+fatal(){
+       	echo -e "${RED}${@}${RESET}"
+}
+
+SucOrFail(){
+	b=$1
+	shift
+	name="$@"
+
+	if [ $b -eq 0 ]
+       	then	
+          pass "${name} Successful"
+	else
+	  fail "${name} Failed"
+	fi
+
+}
+
+info Adding to Build Archive
   go build -o builds/webserver_`date +%F.%T`
-echo Replacing Latest
+info Replacing Latest
   go build -o latest_build/webserver
-echo Building Docker compose
+info Building Docker compose
   docker compose build
-echo Starting server
+info Starting server
   docker compose up -d
-  sleep 10
-echo Testing webapp
-  echo Testing /ping 
-    wget 127.0.0.1:8080/ping -O ./test/tmp
-    if diff ./test/tmp ./test/ping.test;
-      then
-        echo "Ping Test Successful"
-      else
-        echo "Ping Test Failed"
-    fi
-    
-echo Testing Minio
 
-echo Testing Postgres
 
-echo Testing Adminer
+info "Testing webapp"
+
+  info "Testing /healthcheck"
+    count=1
+    until wget 127.0.0.1:8080/health-check -O ./test/tmp  || [ $count -eq 10 ]
+    do
+      let count+=1
+      sleep 1
+    done
+    diff ./test/tmp ./test/healthcheck.test && tmp=0 || tmp=1
+    SucOrFail $tmp "Health_Check_${count} Test"
+    rm -f ./test/tmp
+  echo
+
+  info "Testing /upload"
+    test_file="/home/john/git/easy_storage_db/test/test_upload_files/testing.txt"
+    check_file="/home/john/git/easy_storage_db/test/testing.txt"
+    curl -f -X POST http://localhost:8080/upload \
+      -F "upload[]=@${test_file}"
+    diff $test_file $check_file && tmp=0 || tmp=1
+    SucOrFail $tmp "Upload Test"
+    rm -f $check_file
+  echo
+
+info Testing Minio
+  info "Testing make minio bucket"
+  curl -f -X POST http://localhost:8080/create_bucket/Test-Bucket && tmp=0 || tmp=1
+  SucOrFail $tmp "Create_minio_test_bucket Test"
+  echo
+
+  info "Testing get minio bucket"
+  curl -f -X GET http://localhost:8080/get_bucket/Test-Bucket && tmp=0 || tmp=1
+  SucOrFail $tmp "Get_minio_test_bucket Test"
+  echo
+
+  info "Testing list minio buckets"
+  fatal "No Test Made"
+  echo
+
+  info "Testing add new object"
+  fatal "No Test Made"
+  echo 
+
+  info "Testing get object"
+  fatal "No Test Made"
+  echo
+
+  info "Testing remove object"
+  fatal "No Test Made"
+  echo
+
+  info "Testing remove minio bucket"
+  fatal "No Test Made"
+  echo
+
+#echo Testing Postgres
+
+#echo Testing Adminer
 
 
 
@@ -31,4 +119,4 @@ echo Testing Adminer
 
 
 echo Stopping server
-  docker compose down
+docker compose down
---
+++ run_pipeline.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+log_rotate(){
+  l="../latest_pipeline.log"
+  swp="./swp.log"
+  number=$1
+  let "number-=1"
+  arr=$(seq 1 $number)
+  cd ./pipeline_logs
+  touch $l
+  touch $swp
+  for i in $arr
+    do  
+      file=pipeline_$i.log
+      touch $file
+      cp $file $swp
+      cp $l $file
+      cp $swp $l
+  done
+  cd ..
+}
+
+log_rotate 10
+date > latest_pipeline.log
+./pipeline.sh  &>> latest_pipeline.log
+less -R latest_pipeline.log
